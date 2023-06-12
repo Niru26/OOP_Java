d4aa23af09c9b7772fc9bf114d586106f79a8080
Nikolay <nikolayrudakov@mail.ru>
2023-06-12 21:36:23 +0600

    Workshop 2 commit in-between 30min.

--- workshop_2/src/Animals/Program.java
+++ workshop_2/src/Animals/Program.java
@@ -2,6 +2,54 @@ package Animals;
 
 public class Program {
     public static void main(String[] args) {
+        Cat cat1 = new Cat("Mittens");
+        cat1.voice();
 
+        Dog dog1 = new Dog("Balto");
+        dog1.voice();
     }
 }
+
+    abstract class Animal {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Animal(String name) {
+            this.name = name;
+        }
+        public void jump() {
+            System.out.println("Animal jumped into the air.");
+        }
+        public abstract void voice();
+    }
+
+    class Dog extends Animal {
+        public Dog(String name) {
+            super(name);
+        }
+
+        @Override
+        public void voice() {
+            System.out.println("Dog is barking");
+        }
+    }
+
+    class Cat extends Animal {
+
+        public Cat(String name) {
+            super(name);
+        }
+
+        @Override
+        public void voice() {
+            System.out.println("Cat meows");
+        }
+    }
+
